db93b36cf610fcf4a357375137af5a5895480749
Liv Ghit <livghit@Livs-MacBook-Pro.local>
2023-06-03 12:28:43 +0200

    ✏️  Wrote the lexer nextChar and NextToken func

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "github.com/livghit/gopreter/token"
+
 type Lexer struct {
 	input        string
 	position     int
@@ -22,4 +24,38 @@ func (l *Lexer) readChar() {
 
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSING, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLUM, l.ch)
+	case '(':
+		tok = newToken(token.LPAR, l.ch)
+	case ')':
+		tok = newToken(token.RPAR, l.ch)
+	case '{':
+		tok = newToken(token.LSQURLY, l.ch)
+	case '}':
+		tok = newToken(token.RSQURLY, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '-':
+		tok = newToken(token.MINUS, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
